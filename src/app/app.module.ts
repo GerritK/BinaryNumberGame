4d6dc3c0031694692b6204d44226e79fdb3e49a5
Gerrit Kaul <webmaster@gerritk.net>
2017-10-03 21:25:49 +0200

    add view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {DemoViewComponent} from './views/demo/demo-view.component';
+import {GameViewComponent} from './views/game/game-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoViewComponent,
+    GameViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/views/demo/demo-view.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'bng-demo-view',
+  templateUrl: './demo-view.component.html',
+  styleUrls: ['./demo-view.component.css']
+})
+export class DemoViewComponent {
+
+}
