cb89733bd9a9efb1a4ad30f0d487226d3e308db2
BiancaSophie <bianca.woehler@gmx.net>
2017-10-08 18:21:04 +0200

    - added decimal to hex calculation

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'bng-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.css']
 })
-export class DisplayComponent {
+export class DisplayComponent implements OnChanges {
+  @Input('value') value: number;
 
+  @Input('displayType') displayType: 'dec' | 'hex';
+
+  public displayValue: string;
+
+  public digits: string = '0123456789ABCDEF';
+
+  public digit: number;
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.displayValue = '';
+
+    if (this.displayType === 'dec') {
+
+      this.displayValue = this.value.toString();
+
+    }
+    else if (this.displayType === 'hex') {
+
+      while (this.value > 0) {
+
+        this.digit = this.value % 16;
+
+        this.displayValue = this.digits.charAt(this.digit) + this.displayValue;
+
+        this.value = Math.floor(this.value / 16);
+      }
+    }
+  }
 }
