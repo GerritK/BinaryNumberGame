7b5b584c4a948e3510fcfda347f3861e8e1c1381
Gerrit Kaul <webmaster@gerritk.net>
2017-12-11 17:49:14 +0100

    upgraded to angular 5.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
-import {MatToolbarModule} from '@angular/material';
-import {MatCardModule} from '@angular/material';
-import {MatTabsModule, MatButtonModule, MatButtonToggleModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, MatButtonToggleModule, MatCardModule, MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {DemoViewComponent} from './views/demo/demo-view.component';
